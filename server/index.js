6ae59570cdb5e58fc663325d51060d6b22ac5448
Preethika <preethikasdev@gmail.com>
2025-05-11 22:44:13 +0530

    dist updated

--- server/index.js
+++ server/index.js
@@ -29,9 +29,9 @@ const adminRoute = require('./routes/adminRoute');
 app.use('/admin', adminRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
   });
 } else {
   console.log("In development mode");
