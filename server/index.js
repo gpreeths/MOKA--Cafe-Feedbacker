b5ab588979623aff8e32e55a9221a57be09d1270
Preethika <preethikasdev@gmail.com>
2025-05-12 21:33:33 +0530

    route updated

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,28 +17,27 @@ mongoose.connect(process.env.MONGO_URL, {
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// Static file serving
 app.use('/uploads', express.static(path.join(__dirname, 'uploads', 'reviews')));
-
 app.use('/reviews', (req, res, next) => {
   console.log('Requested image:', req.url);
   next();
 });
 
+// API routes
 const userRoute = require('./routes/userRoute');
 app.use('/user', userRoute);
 
 const adminRoute = require('./routes/adminRoute');
 app.use('/admin', adminRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-  });
-} else {
-  console.log("In development mode");
-}
+// Serve React app (always serve, regardless of NODE_ENV)
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Running on port ${process.env.PORT}`);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Running on port ${process.env.PORT || 5000}`);
 });
