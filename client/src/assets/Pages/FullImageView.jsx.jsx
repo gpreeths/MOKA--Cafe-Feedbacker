a32f98e4bbe264e200cfdc95aabb1bf9cdfe4a10
Preethika <preethikasdev@gmail.com>
2025-05-12 22:50:25 +0530

    updated

--- client/src/assets/Pages/FullImageView.jsx.jsx
+++ client/src/assets/Pages/FullImageView.jsx.jsx
@@ -98,7 +98,7 @@ function FullImageView() {
 
           {review.image && (
             <img
-              src={`${apiUrl}/uploads/${review.image.replace(/^\/+/, '')}`}
+              src={`${apiUrl}/${review.image.replace(/^\/+/, '')}`}
               alt="Full Size Review"
               style={{ maxWidth: '40%', maxHeight: '40%', marginBottom: '1rem' }}
             />
